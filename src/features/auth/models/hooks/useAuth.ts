d3082cbb3c5f66909ee4530b8d36348bf2a2e5cb
Artur-Kogay <9571artur9571@gmail.com>
2025-09-07 22:53:45 +0600

    fix: bags

--- src/features/auth/models/hooks/useAuth.ts
+++ src/features/auth/models/hooks/useAuth.ts
@@ -3,34 +3,38 @@ import type { ISignInForm } from '../types';
 import { useLoading, type IApiError } from '@/shared';
 
 
-type AsyncFunc<FormData, Response> = (data: FormData) => Promise<Response>;
+type AsyncFuncWithData<FormData, Response> = (data: FormData) => Promise<{ data: Response }>;
 
 export const useAuth = () => {
 
     const { isLoading, startLoading, stopLoading } = useLoading();
 
 
-    const onSubmit = useCallback(async <FormData, Response>(formData: FormData | ISignInForm, fc: AsyncFunc<FormData | ISignInForm, Response>) => {
-        startLoading();
-
-        try {
-            const result = await fc(formData);
-            return result?.data;
-        } catch (error: unknown) {
-            const err = error as IApiError;
-
-            switch (err?.status) {
-                case 422:
-                    throw { type: 'server', message: 'Неправильное имя пользователя или пароль.' };
-                case 401:
-                    throw { type: 'server', message: 'Ошибка авторизации' };
-                default:
-                    throw { type: 'server', message: 'Ошибка сервера, попробуйте позже.' };
+    const onSubmit = useCallback(
+        async <FormData, Response>(
+            formData: FormData,
+            fc: AsyncFuncWithData<FormData, Response>
+        ) => {
+            startLoading();
+            try {
+                const result = await fc(formData);
+                return result.data;
+            } catch (error: unknown) {
+                const err = error as IApiError;
+                switch (err?.status) {
+                    case 422:
+                        throw { type: 'server', message: 'Неправильное имя пользователя или пароль.' };
+                    case 401:
+                        throw { type: 'server', message: 'Ошибка авторизации' };
+                    default:
+                        throw { type: 'server', message: 'Ошибка сервера, попробуйте позже.' };
+                }
+            } finally {
+                stopLoading();
             }
-        } finally {
-            stopLoading();
-        }
-    }, []);
+        },
+        []
+    );
 
 
     return { onSubmit, isLoading };
